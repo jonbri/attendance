6733ed29245a3568347902ad5c686ee2a1aab77b
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-01-10 14:05:25 -0500

    inline styles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router";
 import type { Route } from "./+types/home";
 import { attendance } from "~/data";
 
@@ -18,47 +17,62 @@ const averageDaysPerWeekInOffice = (
 ).toFixed(1);
 
 export default function Home() {
+  const aggregateColor =
+    parseInt(averageDaysPerWeekInOffice) >= 3 ? "green" : "red";
   return (
-    <div>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
+    <div style={{ backgroundColor: "lightblue", padding: "30px" }}>
+      <style>
+        {`
+      table {
+        border-collapse: collapse;
+        width: 100%;
+        background-color: white;
+      }
+      th, td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+      }
+      th {
+        background-color: #f2f2f2;
+      }
+      th {
+        background-color: #f2f2f2;
+      }
+      .aggregate {
+         color: ${aggregateColor}
+      }
+      `}
+      </style>
       <table>
         <thead>
           <tr>
             <th>Month</th>
-            <th>Attendance</th>
-            <th>Avg</th>
-            <th>Days</th>
+            <th>Present / Total</th>
+            <th>PCT</th>
+            <th>AVG (days-per-week)</th>
           </tr>
         </thead>
         <tbody>
           {attendance.map(([date, present, total]) => (
             <tr key={date}>
-              <td>
-                <NavLink
-                  to={`/month/${date.toLowerCase().replace(" ", "")}`}
-                  end
-                >
-                  {date}
-                </NavLink>
-              </td>
+              <td>{date}</td>
               <td>{`${present} / ${total}`}</td>
               <td>{`${((present / total) * 100).toFixed(1)}%`}</td>
               <td>{((5 * ((present / total) * 100)) / 100).toFixed(1)}</td>
             </tr>
           ))}
-          <tr className="totals">
-            <td>Total</td>
-            <td>{`${totalInOffice} / ${totalOffice}`}</td>
-            <td>{inOfficePercentage}</td>
-            <td
-              className={
-                parseInt(averageDaysPerWeekInOffice) >= 3 ? "good" : "bad"
-              }
-            >
-              {averageDaysPerWeekInOffice}
-            </td>
+          <tr>
+            <th>Total</th>
+            <th>
+              <strong>{`${totalInOffice} / ${totalOffice}`}</strong>
+            </th>
+            <th>
+              <strong>{inOfficePercentage}</strong>
+            </th>
+            <th className="aggregate">
+              <strong>{averageDaysPerWeekInOffice}</strong>
+            </th>
           </tr>
         </tbody>
       </table>
