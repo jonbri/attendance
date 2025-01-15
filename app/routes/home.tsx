303bc73239f8d03bf60139bf5dc8b834a3801421
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-01-15 16:14:00 -0500

    cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,68 +13,64 @@ const inOfficePercentage = `${inOfficePercentageValue.toFixed(1)}%`;
 const exactRate = (5 * inOfficePercentageValue) / 100;
 const rate = exactRate.toFixed(2);
 
+const brown = "#8B4513";
+const warningYellow = "#f0e68c";
+const lighterGreen = "#98FB98";
+const lightGreen = "#90EE90";
+const middleGreen = "#00FF00";
+const solidGreen = "#006400";
+const thresholdColors = {
+  2.6: warningYellow,
+  2.7: lighterGreen,
+  2.8: lightGreen,
+  2.9: middleGreen,
+  3: solidGreen,
+};
+
+const highestPresent = attendance.reduce(
+  (acc, [, present]) => (present > acc ? present : acc),
+  0,
+);
+const lowestPresent = attendance.reduce(
+  (acc, [, present]) => (present < acc ? present : acc),
+  100,
+);
+
+const highestAbsent = attendance.reduce((acc, [, present, total]) => {
+  const absent = total - present;
+  return absent > acc ? absent : acc;
+}, 0);
+const lowestAbsent = attendance.reduce((acc, [, present, total]) => {
+  const absent = total - present;
+  return absent < acc ? absent : acc;
+}, 100);
+
+const highestRate = attendance.reduce((acc, [, present, total]) => {
+  const rate = (5 * ((present / total) * 100)) / 100;
+  return rate > acc ? rate : acc;
+}, 0);
+const lowestRate = attendance.reduce((acc, [, present, total]) => {
+  const rate = (5 * ((present / total) * 100)) / 100;
+  return rate < acc ? rate : acc;
+}, 100);
+
+const highestPercent = attendance.reduce((acc, [, present, total]) => {
+  const percent = (present / total) * 100;
+  return percent > acc ? percent : acc;
+}, 0);
+const lowestPercent = attendance.reduce((acc, [, present, total]) => {
+  const percent = (present / total) * 100;
+  return percent < acc ? percent : acc;
+}, 100);
+
 export default function Home() {
-  const brown = "#8B4513";
-  const aggregateColor = parseInt(rate) >= 3 ? "green" : brown;
-
-  const highestPresent = attendance.reduce(
-    (acc, [, present]) => (present > acc ? present : acc),
-    0,
-  );
-  const lowestPresent = attendance.reduce(
-    (acc, [, present]) => (present < acc ? present : acc),
-    100,
-  );
-
-  const highestAbsent = attendance.reduce((acc, [, present, total]) => {
-    const absent = total - present;
-    return absent > acc ? absent : acc;
-  }, 0);
-  const lowestAbsent = attendance.reduce((acc, [, present, total]) => {
-    const absent = total - present;
-    return absent < acc ? absent : acc;
-  }, 100);
-
-  const highestRate = attendance.reduce((acc, [, present, total]) => {
-    const rate = (5 * ((present / total) * 100)) / 100;
-    return rate > acc ? rate : acc;
-  }, 0);
-  const lowestRate = attendance.reduce((acc, [, present, total]) => {
-    const rate = (5 * ((present / total) * 100)) / 100;
-    return rate < acc ? rate : acc;
-  }, 100);
-
-  const highestPercent = attendance.reduce((acc, [, present, total]) => {
-    const percent = (present / total) * 100;
-    return percent > acc ? percent : acc;
-  }, 0);
-  const lowestPercent = attendance.reduce((acc, [, present, total]) => {
-    const percent = (present / total) * 100;
-    return percent < acc ? percent : acc;
-  }, 100);
-
-  const warningYellow = "#f0e68c";
-  const lighterGreen = "#98FB98";
-  const lightGreen = "#90EE90";
-  const middleGreen = "#00FF00";
-  const solidGreen = "#006400";
   let backgroundColor = "white";
-  if (exactRate >= 2.6) {
-    backgroundColor = warningYellow;
+  for (const [threshold, color] of Object.entries(thresholdColors)) {
+    if (exactRate >= parseFloat(threshold)) {
+      backgroundColor = color;
+    }
   }
-  if (exactRate >= 2.7) {
-    backgroundColor = lighterGreen;
-  }
-  if (exactRate >= 2.8) {
-    backgroundColor = lightGreen;
-  }
-  if (exactRate >= 2.9) {
-    backgroundColor = middleGreen;
-  }
-  if (exactRate >= 3) {
-    backgroundColor = solidGreen;
-  }
-
+  const aggregateColor = parseInt(rate) >= 3 ? "green" : brown;
   return (
     <div>
       <style>
@@ -106,27 +102,21 @@ export default function Home() {
         color: green;
         font-style: italic;
       }
-      .bad {
+      .bad, .good {
         font-style: italic;
+      }
+      .bad {
         color: red;
       }
       .good {
-        font-style: italic;
         color: green;
-      }
-      .github a {
-          color: blue;
-          font-family: arial;
-          font-size: 0.8rem;
       }
       `}
       </style>
       <table>
         <thead>
           <tr>
-            <th className="github">
-              <a href="https://github.com/jonbri/attendance">GitHub</a>
-            </th>
+            <th></th>
             <th>Pres</th>
             <th>Abs</th>
             <th>Tot</th>
@@ -137,47 +127,34 @@ export default function Home() {
         <tbody>
           {attendance.map(([date, present, total]) => {
             const absent = total - present;
-            const inOfficePercentage = `${((present / total) * 100).toFixed(1)}%`;
+            const inOfficePercentageValue = (present / total) * 100;
+            const inOfficePercentage = `${inOfficePercentageValue.toFixed(1)}%`;
             const rate = (5 * ((present / total) * 100)) / 100;
-
-            const isHighestPresent = present === highestPresent;
-            const isLowestPresent = present === lowestPresent;
-            const isHighestAbsent = absent === highestAbsent;
-            const isLowestAbsent = absent === lowestAbsent;
             const isHighestPercent =
               `${highestPercent.toFixed(1)}%` === inOfficePercentage;
             const isLowestPercent =
               `${lowestPercent.toFixed(1)}%` === inOfficePercentage;
-
-            const isHighestRate = highestRate === rate;
-            const isLowestRate = lowestRate === rate;
-
-            let backgroundColor = isHighestPercent ? "gold" : "white";
-            if (isLowestPercent) {
-              backgroundColor = "white";
-            }
-
             return (
               <tr key={date}>
                 <th
                   className={classnames(
-                    backgroundColor === "gold" ? "highlighted" : undefined,
+                    isHighestPercent ? "highlighted" : undefined,
                   )}
                 >
                   {date}
                 </th>
                 <td
                   className={classnames(
-                    isLowestPresent && "bad",
-                    isHighestPresent && "good",
+                    present === lowestPresent && "bad",
+                    present === highestPresent && "good",
                   )}
                 >
                   {present}
                 </td>
                 <td
                   className={classnames(
-                    isLowestAbsent && "good",
-                    isHighestAbsent && "bad",
+                    absent === lowestAbsent && "good",
+                    absent === highestAbsent && "bad",
                   )}
                 >
                   {absent}
@@ -193,8 +170,8 @@ export default function Home() {
                 </td>
                 <td
                   className={classnames(
-                    isLowestRate && "bad",
-                    isHighestRate && "good",
+                    lowestRate === rate && "bad",
+                    highestRate === rate && "good",
                   )}
                 >
                   {rate.toFixed(1)}
